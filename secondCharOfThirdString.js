30ba2a5a27f2e96a366f13562b7944fc6afd7d24
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-26 10:32:56 -0500

    completed Assignment

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -1,1 +1,22 @@
 const prompt = require('prompt-sync')({sigint: true});
+
+/*
+prompt the user for an array
+log 2nd character in the 3rd item in the array
+IF 3rd item is not a string log an error 
+*/
+
+let inputArray = JSON.parse(prompt("Enter an array: "))
+
+if(inputArray[2] !== undefined && typeof inputArray[2] === 'string'){
+    
+    let str = inputArray[2];
+    if(str.length >= 2){
+
+        console.log("The second character of the third item in the array is " + str.substring(1,2) + ".")
+    }else{
+        console.log("The string doesn't have two or more characters.")
+    }
+}else{
+    console.log("Error")
+}
